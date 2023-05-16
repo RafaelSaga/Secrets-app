6944193db0740812fb0e280a2bc37c83fa3255a0
Rafael Saga <raphaelcaitano@hotmail.com>
2023-05-15 21:53:39 -0300

    Add Hashing security

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const ejs = require('ejs');
 
 const app = express();
 
 console.log(process.env.API_KEY);
-
-// S3_BUCKET = 'YOURS3BUCKET';
-// SECRET_KEY = 'YOURSECRETKEYGOESHERE';
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -26,11 +23,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password'],
-});
-
 const User = new mongoose.model('USer', userSchema);
 
 app.get('/', function (req, res) {
@@ -43,7 +35,7 @@ app.get('/login', function (req, res) {
 
 app.post('/login', function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username })
     .then(function (foundUser) {
@@ -61,7 +53,7 @@ app.get('/register', function (req, res) {
 app.post('/register', function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser
